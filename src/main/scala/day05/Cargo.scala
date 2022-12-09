7f323feb1510bf246d6de0b95d05a76a22649334
Robert Farrimond <robert.farrimond@zego.com>
2022-12-09 15:36:13 +0000

    Move cargo

--- src/main/scala/day05/Cargo.scala
+++ src/main/scala/day05/Cargo.scala
@@ -4,7 +4,11 @@ import Day5.Instruction
 import scala.collection._
 
 case class Cargo(stacks: Map[Int, mutable.Stack[Char]]):
-  def move(instruction: Instruction): Cargo = ???
+  def move(instruction: Instruction): Cargo =
+    (0 until instruction.n).map { _ =>
+      stacks(instruction.to).push(stacks(instruction.from).pop())
+    }
+    this
 
 object Cargo:
   // Assumes well-formed input
@@ -12,7 +16,7 @@ object Cargo:
     val keys = lines.last.replace(" ", "").map(_.toString.toInt)
 
     val initialStack = keys.map(_ -> mutable.Stack.empty[Char]).toMap
-    val stacks = lines.dropRight(1).foldLeft(initialStack) { (acc, next) =>
+    val stacks = lines.dropRight(1).reverse.foldLeft(initialStack) { (acc, next) =>
       (1 to next.length by 4).map(next(_)).zipWithIndex.map {
         case (char, idx) if char != ' ' =>
           acc(idx + 1).push(char)
--- src/test/scala/day05/CargoTest.scala
+++ src/test/scala/day05/CargoTest.scala
@@ -1,23 +1,49 @@
 package day05
 
+import Day5.Instruction
 import munit.FunSuite
-import scala.collection._
+
+import scala.collection.*
 
 class CargoTest extends FunSuite:
 
-  test("Cargo.parse") {
-    val input = List(
-      "    [D]",
-      "[N] [C]",
-      "[Z] [M] [P]",
-      " 1   2   3"
-    )
+  private val input = List(
+    "    [D]",
+    "[N] [C]",
+    "[Z] [M] [P]",
+    " 1   2   3"
+  )
 
+  test("Cargo.parse") {
     val expectedStacks = Map[Int, mutable.Stack[Char]](
-      1 -> mutable.Stack[Char]('Z', 'N'),
-      2 -> mutable.Stack[Char]('M', 'C', 'D'),
-      3 -> mutable.Stack[Char]('P')
+      1 -> mutable.Stack.empty[Char].push('Z', 'N'),
+      2 -> mutable.Stack.empty[Char].push('M', 'C', 'D'),
+      3 -> mutable.Stack.empty[Char].push('P')
     )
 
     assertEquals(Cargo.parse(input), Cargo(expectedStacks))
+  }
+
+  test("Cargo.move should move one crate") {
+    val initialStack = Cargo.parse(input)
+    val instruction: Instruction = Instruction(n = 1, from = 2, to = 1)
+    val expectedStacks = Map[Int, mutable.Stack[Char]](
+      1 -> mutable.Stack.empty[Char].push('Z', 'N', 'D'),
+      2 -> mutable.Stack.empty[Char].push('M', 'C'),
+      3 -> mutable.Stack.empty[Char].push('P')
+    )
+
+    assertEquals(initialStack.move(instruction).stacks, expectedStacks)
+  }
+
+  test("Cargo.move should move multiple crates") {
+    val initialStack = Cargo.parse(input)
+    val instruction: Instruction = Instruction(n = 2, from = 2, to = 1)
+    val expectedStacks = Map[Int, mutable.Stack[Char]](
+      1 -> mutable.Stack.empty[Char].push('Z', 'N', 'D', 'C'),
+      2 -> mutable.Stack.empty[Char].push('M'),
+      3 -> mutable.Stack.empty[Char].push('P')
+    )
+
+    assertEquals(initialStack.move(instruction).stacks, expectedStacks)
   }
